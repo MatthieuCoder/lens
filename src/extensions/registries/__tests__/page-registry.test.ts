1ee7ae60e3d91e9122d31b5edeeecee253caaa8a
Roman <ixrock@gmail.com>
2020-11-15 19:19:19 +0200

    PageRegistration & PageMenuRegistration fixes & more simplification, fix #1383
    Signed-off-by: Roman <ixrock@gmail.com>

--- src/extensions/registries/__tests__/page-registry.test.ts
+++ src/extensions/registries/__tests__/page-registry.test.ts
@@ -1,4 +1,4 @@
-import { getPageUrl, globalPageRegistry } from "../page-registry"
+import { getExtensionPageUrl, globalPageRegistry, PageRegistration } from "../page-registry"
 import { LensExtension } from "../../lens-extension"
 import React from "react";
 
@@ -18,24 +18,25 @@ describe("getPageUrl", () => {
   })
 
   it("returns a page url for extension", () => {
-    expect(getPageUrl(ext)).toBe("/extension/foo-bar")
+    expect(getExtensionPageUrl({ extensionId: ext.name })).toBe("/extension/foo-bar")
   })
 
   it("allows to pass base url as parameter", () => {
-    expect(getPageUrl(ext, "/test")).toBe("/extension/foo-bar/test")
+    expect(getExtensionPageUrl({ extensionId: ext.name, pageId: "/test" })).toBe("/extension/foo-bar/test")
   })
 
   it("removes @", () => {
-    ext.manifest.name = "@foo/bar"
-    expect(getPageUrl(ext)).toBe("/extension/foo-bar")
+    expect(getExtensionPageUrl({ extensionId: "@foo/bar" })).toBe("/extension/foo-bar")
   })
 
   it("adds / prefix", () => {
-    expect(getPageUrl(ext, "test")).toBe("/extension/foo-bar/test")
+    expect(getExtensionPageUrl({ extensionId: ext.name, pageId: "test" })).toBe("/extension/foo-bar/test")
   })
 })
 
 describe("globalPageRegistry", () => {
+  let pages: PageRegistration[];
+
   beforeEach(async () => {
     ext = new LensExtension({
       manifest: {
@@ -46,7 +47,7 @@ describe("globalPageRegistry", () => {
       isBundled: false,
       isEnabled: true
     })
-    globalPageRegistry.add([
+    pages = [
       {
         id: "test-page",
         components: {
@@ -57,12 +58,33 @@ describe("globalPageRegistry", () => {
         id: "another-page",
         components: {
           Page: () => React.createElement('Text')
+        },
+      },
+      {
+        components: {
+          Page: () => React.createElement('Default')
         }
       },
-    ], ext)
+    ];
+    globalPageRegistry.add(pages, ext)
   })
 
   describe("getByPageMenuTarget", () => {
+    it("matching to first registered page without id", () => {
+      const page = globalPageRegistry.getByPageMenuTarget({ extensionId: ext.name })
+      expect(page.id).toEqual(undefined);
+      expect(page.extensionId).toEqual(ext.name);
+      expect(page.routePath).toEqual(getExtensionPageUrl({ extensionId: ext.name }));
+    });
+
+    it("returns matching page", () => {
+      const page = globalPageRegistry.getByPageMenuTarget({
+        pageId: "test-page",
+        extensionId: ext.name
+      })
+      expect(page.id).toEqual("test-page")
+    })
+
     it("returns matching page", () => {
       const page = globalPageRegistry.getByPageMenuTarget({
         pageId: "test-page",
